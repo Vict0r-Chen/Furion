31b829a5944205114d42d116402bc4e1b35e5d83
MonkSoul <monksoul@outlook.com>
2023-05-29 01:08:18 +0800

    😊 初始化 `DependencyInjection` 模块代码范本

---
+++ tests/Furion.Tests/DependencyInjection/DependencyInjectionTests.cs
@@ -0,0 +1,12 @@
+namespace Furion.Tests.DependencyInjection;
+
+/// <summary>
+/// ÒÀÀµ×¢Èë - µ¥Ôª²âÊÔ
+/// </summary>
+public class DependencyInjectionTests
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
