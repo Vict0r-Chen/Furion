78df1abbcd8cb234abd286c6bae905cad83f94b3
MonkSoul <monksoul@outlook.com>
2023-06-09 16:10:26 +0800

    😐 整理 `Usings.cs` 文件

--- framework/Furion.Component.AspNetCore/src/Usings.cs
+++ framework/Furion.Component.AspNetCore/src/Usings.cs
@@ -16,4 +16,5 @@ global using Furion.Component;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
 global using System.Runtime.CompilerServices;
