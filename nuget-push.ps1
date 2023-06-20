aaca8a099168a770473f84028031c58245f687d3
百小僧 <monksoul@outlook.com>
2023-06-20 08:51:03 +0800

    😊 添加框架初始模块文件夹

---
+++ nuget-push.ps1
@@ -0,0 +1,25 @@
+﻿Param(
+    [string] $apikey
+)
+
+cd .\framework\nupkgs;
+$framework_nupkgs = Get-ChildItem -Filter *.nupkg;
+
+for ($i = 0; $i -le $framework_nupkgs.Length - 1; $i++){
+    $item = $framework_nupkgs[$i];
+
+    $nupkg = $item.FullName;
+    $snupkg = $nupkg.Replace(".nupkg", ".snupkg");
+
+    Write-Output "-----------------";
+    $nupkg;
+
+    dotnet nuget push $nupkg --skip-duplicate --api-key $apikey --source https://api.nuget.org/v3/index.json;
+    dotnet nuget push $snupkg --skip-duplicate --api-key $apikey --source https://api.nuget.org/v3/index.json;
+
+    Write-Output "-----------------";
+}
+
+cd ../../;
+
+Write-Warning "Successfully.";
