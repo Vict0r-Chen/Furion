1483fa5707404b5e98378364db6fa2c1486490ab
百小僧 <monksoul@outlook.com>
2023-06-24 14:53:43 +0800

    😐 整理 `GlobalUsings.cs`

--- framework/Furion.Configuration/src/GlobalUsings.cs
+++ framework/Furion.Configuration/src/GlobalUsings.cs
@@ -14,6 +14,9 @@
 
 global using Furion.Configuration;
 global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.Configuration.Ini;
+global using Microsoft.Extensions.Configuration.Json;
+global using Microsoft.Extensions.Configuration.Xml;
 global using Microsoft.Extensions.FileProviders;
 global using Microsoft.Extensions.FileSystemGlobbing;
 global using System.Runtime.CompilerServices;
