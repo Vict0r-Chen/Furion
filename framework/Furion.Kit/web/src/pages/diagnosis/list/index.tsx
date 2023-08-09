41a7eb3da321ec62f08dcdba9174e132def9b692
百小僧 <monksoul@outlook.com>
2023-08-10 03:01:53 +0800

    😐 更新 `Kit` 模块底层代码

--- framework/Furion.Kit/web/src/pages/diagnosis/list/index.tsx
+++ framework/Furion.Kit/web/src/pages/diagnosis/list/index.tsx
@@ -1,5 +1,15 @@
 import { CloseCircleOutlined, WarningOutlined } from "@ant-design/icons";
-import { Card, Popover, Space, Spin, Tag, Typography } from "antd";
+import {
+  Button,
+  Card,
+  Empty,
+  Popover,
+  Skeleton,
+  Space,
+  Spin,
+  Tag,
+  Typography,
+} from "antd";
 import { useLiveQuery } from "dexie-react-hooks";
 import React, { useEffect, useState } from "react";
 import { db } from "../../../db";
@@ -11,15 +21,24 @@ const { Text } = Typography;
 export default function RequestList() {
   const online = useServerStore((state) => state.online);
   const [time, setTime] = useState(new Date());
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const httpDiagnost = useLiveQuery(async () => {
-    return await db.httpDiagnost
-      .where("requestPath")
-      .notEqual("/furion/http-sse")
+    setLoading(true);
+
+    var data = await db.httpDiagnost
+      .filter((f) => f.requestPath !== "/furion/http-sse")
       .reverse()
-      .limit(50)
-      .sortBy("startTimestamp");
-  });
+      .toArray();
+
+    setLoading(false);
+    return data;
+  }, [page]);
+
+  const count = useLiveQuery(async () =>
+    db.httpDiagnost.filter((f) => f.requestPath !== "/furion/http-sse").count()
+  );
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -30,80 +49,99 @@ export default function RequestList() {
   }, []);
 
   if (!httpDiagnost) {
-    return <Spin />;
+    return <Skeleton />;
   }
 
   return (
-    <Card hoverable style={{ width: "100%", margin: "15px 0" }}>
-      <Space direction="vertical">
-        {httpDiagnost?.map((item) => (
-          <div key={item.traceIdentifier}>
-            <Space size={15}>
-              <HttpMethod value={item.requestMethod} />
-              {item.statusCode ? (
-                <Text
-                  type={item.exception ? "danger" : "success"}
-                  style={{ fontWeight: 500 }}
-                >
-                  {item.statusCode}
-                </Text>
-              ) : (
-                <>
-                  {!online ||
-                  (item.startTimestamp &&
-                    (time.getTime() - item.startTimestamp.getTime()) / 1000 >
-                      10) ? (
-                    <WarningOutlined style={{ color: "#faad14" }} />
-                  ) : (
-                    <Spin size="small" />
-                  )}
-                </>
-              )}
-
-              <Popover
-                title={
-                  <Space size={5}>
-                    <Text>连接标识：</Text>
-                    <Text style={{ color: "#595959" }}>
-                      {item.traceIdentifier}
+    <>
+      <Card hoverable style={{ width: "100%", margin: "15px 0" }}>
+        {httpDiagnost.length === 0 ? (
+          <Empty description="暂无数据" />
+        ) : (
+          <Space direction="vertical">
+            {httpDiagnost?.map((item) => (
+              <div key={item.traceIdentifier}>
+                <Space size={15}>
+                  <HttpMethod value={item.requestMethod} />
+                  {item.statusCode ? (
+                    <Text
+                      type={item.exception ? "danger" : "success"}
+                      style={{ fontWeight: 500 }}
+                    >
+                      {item.statusCode}
                     </Text>
-                  </Space>
-                }
-              >
-                <Text
-                  underline
-                  copyable
-                  ellipsis
-                  italic
-                  type={item.exception ? "danger" : undefined}
-                  style={{ fontWeight: 500 }}
-                >
-                  {decodeURIComponent(item.requestPath)}
-                </Text>
-              </Popover>
-              {item.startTimestamp && item.endTimestamp && (
-                <Text type="secondary">
-                  {item.endTimestamp.getTime() - item.startTimestamp.getTime()}
-                  ms
-                </Text>
-              )}
-              <HighlightText
-                type="secondary"
-                italic
-                keyboard
-                $color="#000000A6"
-              >
-                {item.endpoint?.displayName}
-              </HighlightText>
+                  ) : (
+                    <>
+                      {!online ||
+                      (item.startTimestamp &&
+                        (time.getTime() - item.startTimestamp.getTime()) /
+                          1000 >
+                          10) ? (
+                        <WarningOutlined style={{ color: "#faad14" }} />
+                      ) : (
+                        <Spin size="small" />
+                      )}
+                    </>
+                  )}
 
-              {item.exception && (
-                <Tag icon={<CloseCircleOutlined />} color="#f50" />
-              )}
-            </Space>
-          </div>
-        ))}
-      </Space>
-    </Card>
+                  <Popover
+                    title={
+                      <Space size={5}>
+                        <Text>连接标识：</Text>
+                        <Text style={{ color: "#595959" }}>
+                          {item.traceIdentifier}
+                        </Text>
+                      </Space>
+                    }
+                  >
+                    <Text
+                      underline
+                      copyable
+                      ellipsis
+                      italic
+                      type={item.exception ? "danger" : undefined}
+                      style={{ fontWeight: 500 }}
+                    >
+                      {decodeURIComponent(item.requestPath)}
+                    </Text>
+                  </Popover>
+                  {item.startTimestamp && item.endTimestamp && (
+                    <Text type="secondary">
+                      {item.endTimestamp.getTime() -
+                        item.startTimestamp.getTime()}
+                      ms
+                    </Text>
+                  )}
+                  <HighlightText
+                    type="secondary"
+                    italic
+                    keyboard
+                    $color="#000000A6"
+                  >
+                    {item.endpoint?.displayName}
+                  </HighlightText>
+
+                  {item.exception && (
+                    <Tag icon={<CloseCircleOutlined />} color="#f50" />
+                  )}
+                </Space>
+              </div>
+            ))}
+          </Space>
+        )}
+      </Card>
+      {httpDiagnost.length > 0 && (
+        <div
+          style={{ textAlign: "center" }}
+          onClick={() => {
+            setPage((p) => p + 1);
+          }}
+        >
+          {loading && <Skeleton />}
+          <Button>加载更多 {count}</Button>
+        </div>
+      )}
+    </>
   );
 }
 const getColor = (value: string) => {
