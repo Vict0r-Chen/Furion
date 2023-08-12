6e0cb21a410dcc8ac72d9d7d3c502da02ddc895f
百小僧 <monksoul@outlook.com>
2023-08-12 09:24:44 +0800

    😐 调整 `Kit` 模块客户端代码

---
+++ framework/Furion.Kit/kit/src/applications/explore/components/app-card.tsx
@@ -0,0 +1,143 @@
+import { Button, Space, Tag } from "antd";
+import React from "react";
+import { styled } from "styled-components";
+import Flexbox from "../../../components/flexbox";
+import Upward from "../../../components/upward";
+
+const Container = styled.div`
+  box-sizing: border-box;
+  padding: 0 15px 15px 0;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+  }
+
+  @media only screen and (min-width: 481px) and (max-width: 880px) {
+    width: 50%;
+  }
+
+  @media only screen and (min-width: 881px) and (max-width: 1200px) {
+    width: 33.33%;
+  }
+
+  @media (min-width: 1201px) and (max-width: 1590px) {
+    width: 25%;
+  }
+  @media (min-width: 1591px) {
+    width: 16.66%;
+  }
+`;
+
+const Main = styled(Upward)`
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+  height: 250px;
+  border: 1px solid rgb(240, 240, 240);
+  background-color: #fff;
+  border-radius: 8px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  padding: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
+
+  transition: transform 0.3s;
+
+  &:hover {
+    transform: scale(1.03);
+    z-index: 2;
+    box-shadow: 0 2px 8px 2px rgba(0, 0, 0, 0.1);
+  }
+`;
+
+const Panel = styled.div`
+  flex: 1;
+`;
+
+const Banner = styled.div`
+  background-color: rgb(240, 240, 240);
+  height: 110px;
+  border-radius: 8px;
+  margin-bottom: 10px;
+  overflow: hidden;
+`;
+
+const Content = styled(Flexbox)``;
+
+const Logo = styled(Flexbox)`
+  height: 50px;
+  width: 50px;
+  border-radius: 8px;
+  background-color: rgb(240, 240, 240);
+  margin-right: 10px;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  overflow: hidden;
+`;
+
+const Introduction = styled.div`
+  flex: 1;
+  position: relative;
+`;
+
+const Classify = styled(Tag)`
+  position: absolute;
+  right: 0;
+  top: 0;
+`;
+
+const Title = styled.h4`
+  margin: 0;
+`;
+
+const Description = styled.div`
+  font-size: 12px;
+  color: #000000a6;
+`;
+
+interface AppCardProps {
+  title?: string;
+  description?: React.ReactNode;
+  classify?: string;
+  logo?: React.ReactNode;
+  banner?: React.ReactNode;
+  install?: boolean;
+}
+
+const AppCard: React.FC<AppCardProps> = ({
+  title,
+  description,
+  classify,
+  logo,
+  banner,
+  install = false,
+}) => {
+  return (
+    <Container>
+      <Main>
+        <Panel>
+          <Banner>{banner}</Banner>
+          <Content>
+            <Logo>{logo}</Logo>
+            <Introduction>
+              <Classify bordered={false} color="cyan">
+                {classify}
+              </Classify>
+              <Space direction="vertical" size={5}>
+                <Title>{title}</Title>
+                <Description>{description}</Description>
+              </Space>
+            </Introduction>
+          </Content>
+        </Panel>
+        <Button type="primary" disabled={install}>
+          {!install ? "安装" : "已安装"}
+        </Button>
+      </Main>
+    </Container>
+  );
+};
+
+export default AppCard;
--- framework/Furion.Kit/kit/src/applications/explore/local.tsx
+++ framework/Furion.Kit/kit/src/applications/explore/local.tsx
@@ -1,25 +1,48 @@
 import { Space } from "antd";
 import React from "react";
 import { styled } from "styled-components";
+import banner from "../../assets/banner.png";
 import CategoryList from "../../components/category-list";
+import Flexbox from "../../components/flexbox";
+import IconFont from "../../components/iconfont";
+import AppCard from "./components/app-card";
 import Main from "./components/main";
 
-const Container = styled.div``;
+const Container = styled(Flexbox)`
+  margin-top: 25px;
+  margin-left: 10px;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+const Banner = styled.img`
+  height: 100%;
+  width: 100%;
+  display: block;
+`;
 
 const Local: React.FC = () => {
   return (
     <Main>
+      <Space direction="vertical" size={20}>
+        <CategoryList
+          title="分类"
+          items={[
+            { key: "1", label: "全部" },
+            { key: "2", label: "工具" },
+            { key: "3", label: "文档" },
+          ]}
+        />
+      </Space>
       <Container>
-        <Space direction="vertical" size={20}>
-          <CategoryList
-            title="分类"
-            items={[
-              { key: "1", label: "全部" },
-              { key: "2", label: "工具" },
-              { key: "3", label: "文档" },
-            ]}
-          />
-        </Space>
+        <AppCard
+          title="面板"
+          description="在这里可以查看系统概况"
+          classify="工具"
+          logo={<IconFont type="icon-panel" $size={26} />}
+          banner={<Banner src={banner} alt="" />}
+        />
       </Container>
     </Main>
   );
