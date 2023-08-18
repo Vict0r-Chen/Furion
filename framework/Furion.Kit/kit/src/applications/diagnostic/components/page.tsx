067473ab6d9ffc9285719d4057a5db5f7c9d05bc
百小僧 <monksoul@outlook.com>
2023-08-18 12:06:01 +0800

    😐 调整 `Kit` 模块客户端代码

--- framework/Furion.Kit/kit/src/applications/diagnostic/components/page.tsx
+++ framework/Furion.Kit/kit/src/applications/diagnostic/components/page.tsx
@@ -1,10 +1,23 @@
+import { FloatButton } from "antd";
+import React, { useId } from "react";
 import { styled } from "styled-components";
 
-const Page = styled.div`
+const Container = styled.div`
   overflow-y: auto;
   height: calc(100vh - 245px);
   box-sizing: border-box;
   position: relative;
 `;
+
+const Page: React.FC<React.HtmlHTMLAttributes<HTMLDivElement>> = (props) => {
+  const id = useId();
+
+  return (
+    <Container {...props} id={id}>
+      {props.children}
+      <FloatButton.BackTop target={() => document.getElementById(id)!} />
+    </Container>
+  );
+};
 
 export default Page;
--- framework/Furion.Kit/kit/src/home/content/index.tsx
+++ framework/Furion.Kit/kit/src/home/content/index.tsx
@@ -1,5 +1,5 @@
 import { FloatButton } from "antd";
-import React from "react";
+import React, { useId } from "react";
 import { Outlet } from "react-router-dom";
 import { styled } from "styled-components";
 import ContentMain from "./components/content-main";
@@ -11,17 +11,17 @@ const Container = styled.div`
   overflow-y: auto;
 `;
 
-const contentId = "scroll-content";
-
 const Content: React.FC & {
   Title: typeof ContentTitle;
   Menu: typeof ContentMenu;
   Main: typeof ContentMain;
 } = () => {
+  const id = useId();
+
   return (
-    <Container id={contentId}>
+    <Container id={id}>
       <Outlet />
-      <FloatButton.BackTop target={() => document.getElementById(contentId)!} />
+      <FloatButton.BackTop target={() => document.getElementById(id)!} />
     </Container>
   );
 };
