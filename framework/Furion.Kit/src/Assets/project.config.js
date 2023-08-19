85368ed581c7c51084e86281b209cbe21bb96e93
百小僧 <monksoul@outlook.com>
2023-08-20 00:00:49 +0800

    😐 同步 `Kit` 客户端嵌入代码

---
+++ framework/Furion.Kit/src/Assets/project.config.js
@@ -0,0 +1,3 @@
+window.projectConfig = {
+  serverAddress: "https://localhost:7115/furion",
+};
--- framework/Furion.Kit/src/Assets/static/js/865.1db9e6ca.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfurion_kit=self.webpackChunkfurion_kit||[]).push([[865],{865:function(s,e,n){n.r(e),n.d(e,{messages:function(){return u}});var u=JSON.parse('{"1sToVz":"\u9762\u677f"}')}}]);
