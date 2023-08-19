286603272fecda85b80ce124d310525306250312
百小僧 <monksoul@outlook.com>
2023-08-20 02:56:32 +0800

    😐 调整 `Kit` 客户端代码

--- framework/Furion.Kit/kit/src/applications/explore/components/page.tsx
+++ framework/Furion.Kit/kit/src/applications/explore/components/page.tsx
@@ -1,6 +1,10 @@
-import { Button, Dropdown, Space, Tabs, TabsProps } from "antd";
+import { Button, Carousel, Dropdown, Space, Tabs, TabsProps } from "antd";
 import React from "react";
 import { styled } from "styled-components";
+import banner from "../../../assets/banner.png";
+import banner1 from "../../../assets/banner1.jpeg";
+import banner2 from "../../../assets/banner2.jpeg";
+import banner3 from "../../../assets/banner3.png";
 import Category from "../../../components/category";
 import IconFont from "../../../components/iconfont";
 import SearchBox from "../../../components/searchbox";
@@ -49,58 +53,81 @@ const onChange = (key: string) => {
   console.log(key);
 };
 
+const BannerImg = styled.img`
+  height: 280px;
+  width: 100%;
+  /* object-fit: contain; */
+  background-color: #f0f0f0;
+`;
+
 const Page: React.FC = () => {
   return (
-    <Container>
-      <Category
-        title="我的应用"
-        icon={<IconFont type="icon-myapp" $size={16} />}
-      >
-        <My />
-      </Category>
-      <Category
-        title="应用市场"
-        icon={<IconFont type="icon-appmarket" $size={16} />}
-      >
-        <Tabs
-          tabBarExtraContent={{
-            right: (
-              <Space>
-                <SearchBox placeholder="ChatGPT 电商" />
-                <Dropdown
-                  placement="bottomRight"
-                  menu={{
-                    items: [
-                      {
-                        key: 1,
-                        label: "选择应用包",
-                        icon: (
-                          <IconFont
-                            type="icon-package"
-                            $size={16}
-                            color="#000000A6"
-                          />
-                        ),
-                      },
-                    ],
-                  }}
-                >
-                  <Button
-                    type="primary"
-                    icon={<UploadIcon type="icon-upload" $size={16} />}
+    <>
+      <Carousel autoplay autoplaySpeed={10000}>
+        <div>
+          <BannerImg src={banner} alt="" />
+        </div>
+        <div>
+          <BannerImg src={banner1} alt="" />
+        </div>
+        <div>
+          <BannerImg src={banner2} alt="" />
+        </div>
+        <div>
+          <BannerImg src={banner3} alt="" />
+        </div>
+      </Carousel>
+      <Container>
+        <Category
+          title="我的应用"
+          icon={<IconFont type="icon-myapp" $size={16} />}
+        >
+          <My />
+        </Category>
+        <Category
+          title="应用市场"
+          icon={<IconFont type="icon-appmarket" $size={16} />}
+        >
+          <Tabs
+            tabBarExtraContent={{
+              right: (
+                <Space>
+                  <SearchBox placeholder="ChatGPT 电商" />
+                  <Dropdown
+                    placement="bottomRight"
+                    menu={{
+                      items: [
+                        {
+                          key: 1,
+                          label: "选择应用包",
+                          icon: (
+                            <IconFont
+                              type="icon-package"
+                              $size={16}
+                              color="#000000A6"
+                            />
+                          ),
+                        },
+                      ],
+                    }}
                   >
-                    上传
-                  </Button>
-                </Dropdown>
-              </Space>
-            ),
-          }}
-          defaultActiveKey="1"
-          items={items}
-          onChange={onChange}
-        />
-      </Category>
-    </Container>
+                    <Button
+                      type="primary"
+                      icon={<UploadIcon type="icon-upload" $size={16} />}
+                    >
+                      上传
+                    </Button>
+                  </Dropdown>
+                </Space>
+              ),
+            }}
+            defaultActiveKey="1"
+            items={items}
+            onChange={onChange}
+          />
+        </Category>
+      </Container>
+    </>
   );
 };
 
